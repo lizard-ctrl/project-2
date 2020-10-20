b9d6d81a9d824555ef8813434df35ffaae51883c
rors <959655+rors@users.noreply.github.com>
2020-10-20 05:37:03 -0400

    oops, forgot 2 files

--- content.js
+++ content.js
@@ -1,6 +1,15 @@
-var elements = document.getElementsByTagName('p');
+var elements = document.getElementsByTagName('*');
 
 for (var i = 0; i < elements.length; i++) {
    var element = elements[i];
-}
+   // console.log( element );
+   // element.style.setProperty('border','solid 1px red');
 
+   var r = Math.floor(Math.random()*255);
+   var g = Math.floor(Math.random()*255);
+   var b = Math.floor(Math.random()*255);
+   var c = "rgb(" + r + "," + g + "," + b + ")";
+
+   element.style.setProperty('background-color',c);
+
+}
