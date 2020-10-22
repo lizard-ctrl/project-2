b37151b0578cec21250a09759de9b9fa0bd02779
rors <959655+rors@users.noreply.github.com>
2020-10-22 13:06:07 -0400

    experimenting with regular expression text replacement

--- content.js
+++ content.js
@@ -1,15 +1,23 @@
-var elements = document.getElementsByTagName('*');
+// var elements = document.getElementsByTagName('*');
+//
+// for (var i = 0; i < elements.length; i++) {
+//    var element = elements[i];
+//    // console.log( element );
+//    // element.style.setProperty('border','solid 1px red');
+//
+//    var r = Math.floor(Math.random()*255);
+//    var g = Math.floor(Math.random()*255);
+//    var b = Math.floor(Math.random()*255);
+//    var c = "rgb(" + r + "," + g + "," + b + ")";
+//
+//    element.style.setProperty('background-color',c);
+//
+// }
 
-for (var i = 0; i < elements.length; i++) {
-   var element = elements[i];
-   // console.log( element );
-   // element.style.setProperty('border','solid 1px red');
+var html = document.querySelector('html');
+var walker = document.createTreeWalker(html,NodeFilter.SHOW_TEXT);
 
-   var r = Math.floor(Math.random()*255);
-   var g = Math.floor(Math.random()*255);
-   var b = Math.floor(Math.random()*255);
-   var c = "rgb(" + r + "," + g + "," + b + ")";
-
-   element.style.setProperty('background-color',c);
-
+var node;
+while (node = walker.nextNode()) {
+  node.nodeValue = node.nodeValue.replace(/trump/gi,'Some Guy');
 }
